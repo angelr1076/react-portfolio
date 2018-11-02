2ef415d35247d6e031b7f77e2b23f47960bb33a2
Angel <angelr1076@gmail.com>
2018-11-01 23:34:25 -0400

    added updated resume and rebuilt landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -4,51 +4,66 @@ import { Grid, Cell } from 'react-mdl';
 class Landing extends Component {
     render() {
         return (
-        <div style={{width: '100%', margin: 'auto'}}>
-            <Grid className="landing-grid">
-                <Cell col={12}>
-                    <img
-                        src="https://res.cloudinary.com/angelrodriguez/image/upload/c_crop,h_200,w_200,x_105,y_170/v1528903608/261905_157517620988095_7963288_n.jpg" 
-                        alt="avatar-pic"
-                        className="avatar-img"
-                    />
-                    <hr/>
-                    <div className="banner-text">
+            <div className="landing-body">
+                <Grid className="landing-grid">
+                    <Cell col={6}>
+                    <div className="banner-text"></div>
                         <h1>Angel Rodriguez</h1>
                         <h2>Full Stack Web Developer</h2>
 
-                        <hr/>
-
-                        <p>JavaScript | jQuery | React | Sass | ActiveRecord | Node.js | JSON | Phaser3 | Tiled | SQLite3| Ruby | Ruby on Rails Sinatra | Heroku | Firebase | Hugo | Bootsrap | Materialize | HTML5 | CSS3 | Git | Github</p>
-
                         <div className="social-links">
+                            {/* LinkedIn */}
+                            <a href="https://www.linkedin.com/in/angelrodriguezlead/" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                            </a>
 
-                        {/* LinkedIn */}
-                        <a href="https://www.linkedin.com/in/angelrodriguezlead/" rel="noopener noreferrer" target="_blank">
-                            <i className="fa fa-linkedin-square" aria-hidden="true" />
-                        </a>
-                        
-                        {/* Github */}
-                        <a href="https://github.com/angelr1076" rel="noopener noreferrer" target="_blank">
-                            <i className="fa fa-github-square" aria-hidden="true" />
-                        </a>
+                            {/* Github */}
+                            <a href="https://github.com/angelr1076" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-github-square" aria-hidden="true" />
+                            </a>
 
-                        {/* Medium */}
-                        <a href="https://medium.com/@angelr1076" rel="noopener noreferrer" target="_blank">
-                            <i className="fa fa-medium" aria-hidden="true" />
-                        </a>
-
-                        {/* Twitter */}
-                        <a href="https://twitter.com/AngelRoddy7" rel="noopener noreferrer" target="_blank">
-                            <i className="fa fa-twitter-square" aria-hidden="true" />
-                        </a>
+                            {/* Twitter */}
+                            <a href="https://twitter.com/AngelRoddy7" rel="noopener noreferrer" target="_blank">
+                                <i className="fa fa-twitter-square" aria-hidden="true" />
+                            </a>
                         </div>
-                    </div>
-                </Cell>
-            </Grid>
-        </div>
+
+                        <hr/>
+                        
+                        <div className="devicons">
+                            <i className="devicon-javascript-plain colored" />
+                            <i className="devicon-jquery-plain-wordmark colored" />
+                            <i className="devicon-react-original colored" />
+                            <i className="devicon-ruby-plain-wordmark colored" />
+                            <i className="devicon-rails-plain-wordmark colored" />
+                            <i className="devicon-bootstrap-plain-wordmark" />
+                            <i className="devicon-html5-plain-wordmark colored" />
+                            <i className="devicon-css3-plain-wordmark colored" />
+                            <i className="devicon-sass-original colored" />
+                            <i className="devicon-visualstudio-plain-wordmark" />
+                            <i className="devicon-nodejs-plain" />
+                            <i className="devicon-amazonwebservices-plain-wordmark" />
+                            <i className="devicon-heroku-original-wordmark colored" />
+                            <i className="devicon-github-plain-wordmark" />
+                        </div>
+                        <p>JavaScript | jQuery | React | Sass | ActiveRecord | Node.js | JSON | Phaser3 | Tiled | SQLite3| Ruby | Ruby on Rails | Sinatra | Heroku | Firebase | Hugo | Bootsrap | Materialize | HTML5 | CSS3 | Git | Github</p>
+                    </Cell>
+
+                    <Cell col={6}>
+                        <div className="landing-bio">
+                            <p style={{width: '75%', margin: '10px', paddingTop: '1em'}}>Hello, my name's Angel! Thanks for checking out my website! After years in the food service industry, I decided to follow my true passion of building things, breaking them, and rebuilding the things I broke only to make them better! A little over a year ago, I decided I needed a drastic change in my life. I came to realize the food industry I once loved so much was never truly my number one passion.</p>
+
+                            <p style={{width: '75%', margin: '10px', paddingTop: '1em'}}>I've always been fascinated by tech, in some shape or form. Whether it was building a menu on my laptop, running price changes in a database or livening up a personal website with some HTML. I have been coding for over a year now, and recently, I decided to take a deep dive into a web/software development bootcamp. I realized this was one of the greatest decisions I have ever made. It was also the best investment I could ever make in myself. My passion has always truly been problem solving and tech is one of the best ways to demonstrate problem solving skills. Yes, programming can be difficult; it can test your will and spirit. However, I feel it has shown me what I'm truly made of and how much I can accomplish if I don't give up and simply keep learning and working on new projects.</p> 
+
+                            <p style={{width: '75%', margin: '10px', paddingTop: '1em'}}>I don't have a preference from back or front-end work. I enjoy both and my approach to either one is that I will learn more as I handle each end. I've worked in training many people on the job, so I'm no stranger to dealing with a vast array of personalities.</p> 
+
+                            <p style={{width: '75%', margin: '10px', paddingTop: '1em'}}>If you feel that I could be a good fit at your company, please call or email me.</p>
+                        </div>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
-
+    
 export default Landing;
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,88 +1,18 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-import Education from './education';
-import Experience from './experience';
+
+
 class Resume extends Component {
     render() {
         return (
             <div>
                 <Grid>
-                    <Cell col={4}>
-                        <div style={{textAlign: 'center'}}>
+                    <Cell col={6}>
+                        <div className="resume-template">
                             <img
-                                src="https://res.cloudinary.com/angelrodriguez/image/upload/v1528903608/3_2.jpg"
-                                alt="avatar"
-                                style={{height: '300px'}}
+                                src="https://res.cloudinary.com/angelrodriguez/image/upload/q_100/v1541122983/angel-rodriguez-resume-digital.png"
+                                alt="resume"
                             />
-                        </div>
-                        <h4 style={{color: 'grey'}}>Web Developer</h4>
-                        <hr style={{borderTop: '3px solid #83bfb2', width: '50%'}} />
-                        <p>I've worked with JavaScript, jQuery, React, Sass, ActiveRecord, Node.js, JSON, Phaser3, Tiled, SQLite3, Ruby, Rails, Sinatra, Heroku, Firebase, Hugo, Bootsrap, Materialize, HTML5, CSS3, Git and Github</p>
-                        <hr style={{borderTop: '3px solid #83bfb2', width: '50%'}} />
-                        <h5>Location</h5>
-                        <p>Philadelphia, PA</p>
-                        <h5>Phone</h5>
-                        <p>858.232.3288</p>
-                        <h5>Email</h5>
-                        <p>angelr1076@gmail.com</p>
-                        <h5>Web</h5>
-                        <p><a rel="noopener noreferrer" target="_blank" href="https://www.angelroddy.com">angelroddy.com</a></p>
-                        <hr style={{borderTop: '3px solid #83bfb2', width: '50%'}} />
-
-                    </Cell>
-                    <Cell className="resume-right-col" col={8}>
-                    <h2>Experience</h2>
-
-                        <Experience 
-                            startYear={2016}
-                            endYear={2017}
-                            jobName="Nationwide"
-                            jobTitle="Commercial Lines Processor"
-                            jobDescription="Initiated a root cause problem-solving objective to target an issue within our origination process that caused a major blockage affecting 100s of agents within the company. Successfully targeted the issue and merged with members of the IT department to finally resolve the impediment. Assigned to special projects to update reference documents in our database that were outdated and required images and modernization of wording and references."
-                        />
-
-                        <Experience 
-                            startYear={2012}
-                            endYear={2016}
-                            jobName="Wawa, Inc."
-                            jobTitle="Food & Beverage Manager"
-                            jobDescription="Led stores to earn Food Safety Risk Assessments and Business Readiness scores in the top 30%. Achieved 50% sales increases in express food service gross profits by implementing new merchandising techniques, such as prominently displaying and sampling slow moving products. Appointed Food Service Efficiency Training by Area Manager, tasked with training new managers."
-                        />
-
-                        <h2>Education</h2>
-
-                        <Education 
-                            startYear={2018}
-                            endYear={2018}
-                            schoolName="New York Code & Design Academy"
-                            degree="Certification - Web Development/Software Engineering"
-                            schoolDescription="Completed 420+ hours of Web Development Intensive Program covering various full-stack technologies and various methodologies including OOP, TDD, pair-programming, AGILE, and SCRUM."
-                        />
-
-                        <Education 
-                            startYear={2008}
-                            endYear={2011}
-                            schoolName="Walnut Hill College"
-                            degree="Bachelor of Science in Culinary Arts & Hospitality Studies"
-                            schoolDescription="Bachelor of Science in Culinary Arts & Hospitality Studies
-                            Graduated Summa Cum Laude. Traveled to several destinations in Europe and studied in both class room environments and hands on training in culinary and hospitality studies."
-                        />
-                        <hr style={{borderTop: '3px solid #e22947'}} />
-
-                    <h2>Skills</h2>
-                        <div className="devicons">
-                            <i className="devicon-javascript-plain colored" />
-                            <i className="devicon-jquery-plain-wordmark colored" />
-                            <i className="devicon-react-original colored" />
-                            <i className="devicon-ruby-plain-wordmark colored" />
-                            <i className="devicon-rails-plain-wordmark colored" />
-                            <i className="devicon-bootstrap-plain-wordmark" />
-                            <i className="devicon-html5-plain-wordmark colored" />
-                            <i className="devicon-css3-plain-wordmark colored" />
-                            <i className="devicon-sass-original colored" />
-                            <i className="devicon-visualstudio-plain colored" />
-                            <i className="devicon-heroku-original-wordmark colored" />
-                            <i className="devicon-github-plain-wordmark" />
                         </div>
                     </Cell>
                 </Grid>
