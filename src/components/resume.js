5f03429904ab07e731b58c61b2735a0867c41056
Angel <angelr1076@gmail.com>
2018-11-02 08:46:10 -0400

    adjusted resume size and background layout

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
-
-
 class Resume extends Component {
     render() {
         return (
             <div>
-                <Grid>
-                    <Cell col={6}>
-                        <div className="resume-template">
-                            <img
-                                src="https://res.cloudinary.com/angelrodriguez/image/upload/q_100/v1541122983/angel-rodriguez-resume-digital.png"
-                                alt="resume"
-                            />
-                        </div>
-                    </Cell>
-                </Grid>
+                <div className="resume-template">
+                    <img
+                        src="https://res.cloudinary.com/angelrodriguez/image/upload/q_100/v1541122983/angel-rodriguez-resume-digital.png"
+                        alt="resume"
+                        style={{width: '65%', height: '70%'}}
+                    />
+                </div>
             </div>
         )
     }
