29c308a9892c0938296edbce1f37ecd6c24c974b
Angel <angelr1076@gmail.com>
2018-11-03 13:20:19 -0400

    changed ease-in/ease-out timing

--- src/components/main.js
+++ src/components/main.js
@@ -13,11 +13,7 @@ function Main({ location }) {
           <TransitionGroup className="transition-group">
             <CSSTransition
               key={location.key}
-<<<<<<< HEAD
-              timeout={{ enter: 400, exit: 400 }}
-=======
               timeout={{ enter: 300, exit: 300 }}
->>>>>>> 57d1f50325f850237582f824358faec1d4278e3e
               classNames={ 'fade'}
             >
               <section className="route-section">
@@ -40,11 +36,7 @@ function Main({ location }) {
     }
     .fade-enter.fade-enter-active {
         opacity: 1;
-<<<<<<< HEAD
-        transition: opacity 400ms ease-in;
-=======
         transition: opacity 300ms ease-in;
->>>>>>> 57d1f50325f850237582f824358faec1d4278e3e
     }
     .fade-exit {
         opacity: 1;
@@ -52,11 +44,7 @@ function Main({ location }) {
       
     .fade-exit.fade-exit-active {
         opacity: 0.01;
-<<<<<<< HEAD
-        transition: opacity 400ms ease-in;
-=======
         transition: opacity 300ms ease-in;
->>>>>>> 57d1f50325f850237582f824358faec1d4278e3e
     }
     
     div.transition-group {
