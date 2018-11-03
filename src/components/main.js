6b3ee52c7160d295441a2e71b21089b222b1feca
Angel <angelr1076@gmail.com>
2018-11-03 13:17:15 -0400

    added page transitions react transition group and styled components

--- src/components/main.js
+++ src/components/main.js
@@ -1,17 +1,61 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import styled from "styled-components";
+import { Switch, Route, withRouter } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import LandingPage from './landingpage';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
 
-const Main = () => (
-    <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/resume" component={Resume} />
-    </Switch>
-)
+function Main({ location }) {
+    return (
+        <Wrapper>
+          <TransitionGroup className="transition-group">
+            <CSSTransition
+              key={location.key}
+              timeout={{ enter: 400, exit: 400 }}
+              classNames={ 'fade'}
+            >
+              <section className="route-section">
+                <Switch location={location}>
+                    <Route exact path="/" component={LandingPage} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/projects" component={Projects} />
+                    <Route path="/resume" component={Resume} />
+                </Switch>
+            </section>
+          </CSSTransition>
+        </TransitionGroup>
+      </Wrapper>
+    );
+  }
 
-export default Main;
+  const Wrapper = styled.div`
+    .fade-enter {
+        opacity: 0.01;
+    }
+    .fade-enter.fade-enter-active {
+        opacity: 1;
+        transition: opacity 400ms ease-in;
+    }
+    .fade-exit {
+        opacity: 1;
+    }
+      
+    .fade-exit.fade-exit-active {
+        opacity: 0.01;
+        transition: opacity 400ms ease-in;
+    }
+    
+    div.transition-group {
+      position: relative;
+ }
+ section.route-section {
+   position: absolute;
+   width: 100%;
+   top: 0;
+   left: 0;
+ }
+`;
+
+export default withRouter(Main);
