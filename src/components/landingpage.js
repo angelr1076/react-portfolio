a366f03bdb54133b290f6db4e61f89d6c74a09bd
Angel <angelr1076@gmail.com>
2018-11-20 14:25:08 -0500

    adjusted landing page wording

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -56,13 +56,13 @@ class Landing extends Component {
                         <div className="landing-bio">
                             <p style={{width: '75%', margin: '5%'}}>Greetings, and thanks for checking out my website! I'm a web developer that is passionate about learning new things each and every day! In fact, since I have been coding a little over a year ago, I can say there hasn't been a day that I haven't had the opportunity to learn something new. I chose programming/web development as a way to solve problems.</p>
 
-                            <p style={{width: '75%', margin: '5%'}}>Prior to learning to program, I had many ideas that I wanted to put into action, but I had no clue where and how to start. At the time where I discovered that you didn't have to have a degree in computer science to solve technical problems, I realized I could empower myself, and others to learn to bring their ideas to life by chaining data together through code. </p> 
+                            <p style={{width: '75%', margin: '5%'}}>Prior to learning to program, I had many ideas that I wanted to put into action, but I had no clue where and how to start. At the time when I discovered that you didn't have to have a degree in computer science to solve technical problems, I realized I could empower myself, and others to learn to bring their ideas to life by chaining data together through code. </p> 
 
                             <p style={{width: '75%',  margin: '5%'}}>When I have an idea that I'm able to sketch out, plan and bring to life to view and interact with, it is like no other feeling I have experienced in my career in any other field. It is such a powerful, and empowering feeling to create, break, and improve your own creation through code, and also learn from doing that in the process.</p>
                             
                             <p style={{width: '75%',  margin: '5%'}}>Aside from programming, my creativity also extends to cooking. I love to form new recipes, cook then serve them to family and friends. </p>
 
-                            <p style={{width: '75%', margin: '5%'}}>I would love the opportunity to speak with you if you have questions about my background or if you'd like to interview me and maybe talk about food or tech. Thank you!</p>
+                            <p style={{width: '75%', margin: '5%'}}>I would love the opportunity to speak with you if you have questions about my background or if you'd like to interview me and maybe talk about food or tech. Thanks again!</p>
                         </div>
                     </Cell>
                 </Grid>
