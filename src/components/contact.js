8b893d6e7b3b027eaa98138deb1558b8a22bd10c
Angel <angelr1076@gmail.com>
2018-11-28 09:49:52 -0500

    changed landing and contact pages

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Grid, Cell, List, ListItemContent, ListItem } from 'react-mdl';
 
+
 class Contact extends Component {
     render() {
         return (
@@ -14,7 +15,7 @@ class Contact extends Component {
                         style={{ width: '80%', height: 'auto' }}
                     />
 
-                    <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>Web developer with a passion for front-end design and back-end architecture. I've worked with many tech tools, including: JavaScript, React, jQuery, Sass, AWS,ActiveRecord, Node.js, JSON, Phaser3, Tiled, SQLite3, Ruby, Ruby on Rails, Sinatra, Heroku, Firebase, Hugo, Bootsrap, Materialize, HTML5, CSS3, Git and Github. Contact me if you like my work and would like to hire me. You can either email me at angelr1076@gmail.com or contact me on social media.</p>
+                    <p style={{width: '75%', margin: 'auto', paddingTop: '1em'}}>I'm a web developer with a passion for front and back-end design. I've worked with: JavaScript, React, jQuery, Sass, AWS, ActiveRecord, Node.js, JSON, Phaser3, Tiled, SQLite3, Ruby, Ruby on Rails, Sinatra, Heroku, Firebase, Hugo, Bootsrap, Materialize, HTML5, CSS3, Git and Github. Please contact me if you have any questions or advice. My contact information is listed on this page so you can get a hold of me any way you'd like.</p>
                     </Cell>
                     <Cell col={6}>
                         <h2>Contact Me</h2>
