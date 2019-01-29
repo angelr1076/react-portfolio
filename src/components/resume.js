f2d2900ad5145d9c83073810aa2f56325134f2d8
Angel <angelr1076@gmail.com>
2019-01-28 20:26:31 -0500

    adjusted cv sizing and added new cv

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import './Resume.css';
+import React, { Component } from 'react'
+import './Resume.css'
 
 class Resume extends Component {
-    render() {
-        return (
-            <div>
-                <div className="resume-template">
-                    <img
-                        src="https://res.cloudinary.com/angelrodriguez/image/upload/v1546441709/rodriguez_angel_resume.png"
-                        alt="resume"
-                        style={{ width: '70%', height: '70%'}}
-                    />
-                </div>
-            </div>
-        )
-    }
+  render () {
+    return (
+      <div>
+        <div className='resume-template'>
+          <img
+            src='https://res.cloudinary.com/angelrodriguez/image/upload/v1548725111/rodriguez_angel_resume.png'
+            alt='resume'
+            style={{ width: '60%', height: '70%' }}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Resume;
+export default Resume
