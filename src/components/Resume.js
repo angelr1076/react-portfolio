20a47d5f35c5dd160ba3a948ddb02eed985c4464
Angel Rodriguez <angelr1076@gmail.com>
2019-03-12 11:10:39 -0400

    removed navbar texture.svg and degree of light

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
     <div>
       <div className="resume-template">
         <img
-          src="https://res.cloudinary.com/angelrodriguez/image/upload/v1549040558/resume_image.png"
+          src="https://res.cloudinary.com/angelrodriguez/image/upload/v1552403113/rodriguez_angel_resume_blue.png"
           alt="resume"
           style={{ width: "60%", paddingTop: "2rem" }}
         />
